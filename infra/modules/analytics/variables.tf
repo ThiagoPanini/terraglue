4d6a196dd18221236b6efe02c27b09c0fbee800d
Thiago Panini <panini.development@gmail.com>
2022-10-01 00:14:32 -0300

    Incluindo variável geral com todos os mapeamentos de criação de databases e tabelas no catálogo

--- infra/modules/analytics/variables.tf
+++ infra/modules/analytics/variables.tf
@@ -10,12 +10,12 @@ de variáveis para toda a construção do ambiente de
 utilização do Glue na AWS
 -------------------------------------------------- */
 
-variable "local_data_path" {
-  description = "Caminho local de armazenamento dos arquivos a serem inseridos no bucket s3"
-  type        = string
-}
-
 variable "glue_databases" {
   description = "Referências dos databases a serem criados no catálogo de dados do Glue para recebimento das tabelas"
   type        = list(string)
+}
+
+variable "glue_catalog_map" {
+  description = "Dicionário contendo todas as informações necessárias para inserção de entradas (tabelas) no catálogo de dados do Glue"
+  type        = map(any)
 }
