bc3dc038ab719f3b041963a60fe5a0e3710eb0fc
Thiago Panini <panini.development@gmail.com>
2022-10-01 00:12:57 -0300

    Incluindo for_each para criação de bucket para o athena e para o glue

--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -11,18 +11,21 @@ de dados a serem utilizadas como origens do script
 glue implantado.
 
 RESOURCES: Os recursos aqui implantados serão:
-  - Bucket S3
+  - Bucket S3 para dados soR
+  - Bucket S3 para query results do Athena
   - Arquivos de fontes de dados com prefixos
 -------------------------------------------------- */
 
 # Definindo bucket s3
-resource "aws_s3_bucket" "sor" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "this" {
+  for_each = var.bucket_names_map
+  bucket   = each.value
 }
 
 # Definindo bloqueio de acesso público ao bucket
 resource "aws_s3_bucket_public_access_block" "all_private" {
-  bucket = aws_s3_bucket.sor.id
+  for_each = var.bucket_names_map
+  bucket   = aws_s3_bucket.this[each.key].id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -32,7 +35,8 @@ resource "aws_s3_bucket_public_access_block" "all_private" {
 
 # Criptografando bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.sor.bucket
+  for_each = var.bucket_names_map
+  bucket   = aws_s3_bucket.this[each.key].bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
@@ -43,7 +47,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 # Adicionando arquivos locais como tabelas
 resource "aws_s3_object" "data_sources" {
   for_each = var.flag_upload_data_files ? fileset(var.local_data_path, "**") : []
-  bucket   = aws_s3_bucket.sor.bucket
+  bucket   = aws_s3_bucket.this["sor"].bucket
   key      = each.value
   source   = "${var.local_data_path}${each.value}"
   #etag                   = filemd5("${var.local_data_path}${each.value}")
