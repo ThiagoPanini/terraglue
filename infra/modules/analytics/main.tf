4be47405bc5943d82ca1e0c141488aaecdd53540
Thiago Panini <panini.development@gmail.com>
2022-10-01 00:14:05 -0300

    Incluindo criação de database e tabelas (sem schema) no catálogo de dados do Glue

--- infra/modules/analytics/main.tf
+++ infra/modules/analytics/main.tf
@@ -11,9 +11,50 @@ RESOURCES: Os recursos aqui implantados serão:
   - Databases no Glue Data Catalog
 -------------------------------------------------- */
 
-# Criando database no glue catalog
-resource "aws_glue_catalog_database" "source" {
+# Testando criação de databases
+resource "aws_glue_catalog_database" "all" {
   for_each    = toset(var.glue_databases)
   name        = each.value
-  description = "Database para alocação de tabelas do schema ${each.value}"
+  description = "Database ${each.value} criado para simular schema de tabelas na criação de jobs do Glue"
+}
+
+# Testando a criação de tabelas
+resource "aws_glue_catalog_table" "all" {
+  for_each      = var.glue_catalog_map
+  database_name = each.value.database
+  name          = each.value.table_name
+  description   = "Entrada para tabela ${each.value.database}.${each.value.table_name} contendo dados a serem utilizados em jobs do Glue"
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "EXTERNAL"               = "TRUE"
+    "skip.header.line.count" = "1"
+  }
+
+  storage_descriptor {
+    location      = each.value.location
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "csv_stream"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+
+      parameters = {
+        "separatorChar" = ","
+        "quoteChar"     = "\"",
+        "escapeChar"    = "\\"
+      }
+    }
+
+    columns {
+      name = each.value.columns[0]
+    }
+
+  }
+
+  depends_on = [
+    aws_glue_catalog_database.all
+  ]
 }
