a40311c13e12f443df690e24f6066b4b510298de
Thiago Panini <panini.development@gmail.com>
2022-10-15 14:08:22 -0300

    feat: implementando módulo iam com criação de políticas e role

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -29,5 +29,20 @@ data "aws_iam_policy_document" "glue_trust" {
   }
 }
 
+# Criando políticas do IAM através de arquivos JSON
+resource "aws_iam_policy" "glue_policies" {
+  for_each = fileset(var.iam_policies_path, "**")
+  name     = split(".", each.value)[0]
+  policy   = file("${var.iam_policies_path}/${each.value}")
+}
+
+# Criando role de acesso do Glue
+resource "aws_iam_role" "glue_role" {
+  name               = "glue-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.glue_trust.json
+  managed_policy_arns = [
+    for p in aws_iam_policy.glue_policies : p.arn
+  ]
+}
 
 
