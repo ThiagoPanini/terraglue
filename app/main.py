e6191f23f43816a1eaa067f7a1195855f5e4f451
Thiago Panini <panini.development@gmail.com>
2022-10-16 23:20:33 -0300

    feat: primeira versão de script de job do glue funcional na aws

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ TABLE OF CONTENTS:
     2.2 Preparando dados de pagamentos
     2.3 Preparando dados de reviews de pedidos
     2.4 Gerando base final a partir de joins
-
+    2.5 Escrita de base final em bucket do s3
 ------------------------------------------------------
 
 ------------------------------------------------------
@@ -38,11 +38,16 @@ TABLE OF CONTENTS:
 ---------------------------------------------------"""
 
 # Bibliotecas da aplicação
-from pyspark.sql import SparkSession
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql.functions import col, count, avg,\
-    sum, round, count_distinct, max
-from src.local import read_local_data
+    sum, round, countDistinct, max, lit
 import logging
+from datetime import datetime
 
 
 """---------------------------------------------------
@@ -57,9 +62,9 @@ logger.setLevel(logging.DEBUG)
 # Configurando formato das mensagens no objeto
 log_format = "%(levelname)s;%(asctime)s;%(filename)s;"
 log_format += "%(lineno)d;%(message)s"
-date_format = "%Y-%m-%d %H:%M:%S"
+log_date_format = "%Y-%m-%d %H:%M:%S"
 formatter = logging.Formatter(log_format,
-                              datefmt=date_format)
+                              datefmt=log_date_format)
 
 # Configurando stream handler do objeto de log
 stream_handler = logging.StreamHandler()
@@ -72,15 +77,38 @@ logger.addHandler(stream_handler)
         1.3 Definindo variáveis da aplicação
 ---------------------------------------------------"""
 
-# Criando sessão Spark
-logger.debug("Criando e obtendo objeto de sessão Spark")
+# Definindo partição
+PARTITION_NAME = "anomesdia_hms"
+PARTITION_FORMAT = "yyyyMMdd_HHmmss"
+
+# Variáveis de escrita da tabela final
+OUTPUT_BUCKET = "aws-sot-data-739870437205-us-east-1"
+OUTPUT_DB = "ra8"
+OUTPUT_TABLE = "tbsot_ecommerce_br"
+OUTPUT_PATH = f"s3://{OUTPUT_BUCKET}/{OUTPUT_DB}/{OUTPUT_TABLE}"
+CONNECTION_TYPE = "s3"
+UPDATE_BEHAVIOR = "UPDATE_IN_DATABASE"
+PARTITION_KEYS = [PARTITION_NAME]
+COMPRESSION = "snappy"
+ENABLE_UPDATE_CATALOG = True
+
+# Preparando elementos da aplicação Spark
+logger.debug("Criando e obtendo elementos da Spark Application")
 try:
-    spark = SparkSession\
-        .builder\
-        .appName("ecommerce-data-processing")\
-        .getOrCreate()
+    # Coletando argumentos da aplicação
+    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+    # Gerando SparkContext e GlueContext
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+
+    # Gerando job no contexto do Glue com argumentos coletados
+    job = Job(glueContext)
+    job.init(args['JOB_NAME'], args)
 except Exception as e:
-    logger.error(f"Erro ao obter objeto de sessão Spark. Exception: {e}")
+    logger.error(f"Erro ao obter elementos da aplicação. Exception: {e}")
+    raise e
 
 
 """---------------------------------------------------
@@ -88,10 +116,39 @@ except Exception as e:
           2.1 Lendo dados e obtendo DataFrames
 ---------------------------------------------------"""
 
-# Obtendo DataFrames a serem utilizados
-df_orders, df_reviews,\
-    df_payments, df_customers, _ = read_local_data(spark=spark)
+# Criando DynamicFrame: orders
+dynamicf_orders = glueContext.create_dynamic_frame.from_catalog(
+    database="ra8",
+    table_name="orders",
+    transformation_ctx="dynamicf_orders",
+)
 
+# Criando DynamicFrame: customers
+dynamicf_customers = glueContext.create_dynamic_frame.from_catalog(
+    database="ra8",
+    table_name="customers",
+    transformation_ctx="dynamicf_customers",
+)
+
+# Criando DynamicFrame: payments
+dynamicf_payments = glueContext.create_dynamic_frame.from_catalog(
+    database="ra8",
+    table_name="payments",
+    transformation_ctx="dynamicf_payments",
+)
+
+# Criando DynamicFramde: reviews
+dynamicf_reviews = glueContext.create_dynamic_frame.from_catalog(
+    database="ra8",
+    table_name="reviews",
+    transformation_ctx="dynamicf_reviews",
+)
+
+# Convertendo DynamicFrames em DataFrames
+df_orders = dynamicf_orders.toDF()
+df_customers = dynamicf_customers.toDF()
+df_payments = dynamicf_payments.toDF()
+df_reviews = dynamicf_reviews.toDF()
 
 """---------------------------------------------------
 -------- 2. TRANSFORMAÇÕES NA APLICAÇÃO SPARK --------
@@ -113,7 +170,7 @@ try:
         count("order_id").alias("installments"),
         round(sum("payment_value"), 2).alias("sum_payments"),
         round(avg("payment_value"), 2).alias("avg_payment_value"),
-        count_distinct("payment_type").alias("distinct_payment_types")
+        countDistinct("payment_type").alias("distinct_payment_types")
     )
 
     # Enriquecendo base de pagamentos com tipo de pagamento mais comum
@@ -135,6 +192,7 @@ try:
 except Exception as e:
     logger.error("Erro ao preparar DAG de transformações para dados " +
                  f"de pagamentos. Exception: {e}")
+    raise e
 
 
 """---------------------------------------------------
@@ -161,6 +219,7 @@ try:
 except Exception as e:
     logger.error("Erro ao preparar DAG de transformações " +
                  f"de reviews de pedidos. Exception: {e}")
+    raise e
 
 
 """---------------------------------------------------
@@ -186,4 +245,67 @@ try:
     ).drop(df_reviews_prep.order_id)
 except Exception as e:
     logger.error(f"Erro ao preparar DAG para tabela final. Exception: {e}")
+    raise e
 
+logger.info("Adicionando coluna de partição como parte das transformações")
+try:
+    df_sot_ecommerce_partitioned = df_sot_ecommerce\
+        .withColumn(PARTITION_NAME,
+                    lit(datetime.now().strftime("%Y%m%d_%H%M%S")))
+except Exception as e:
+    logger.error("Erro ao adicionar coluna de partição no DataFrame " +
+                 f"Exception: {e}")
+    raise e
+
+
+"""---------------------------------------------------
+-------- 2. TRANSFORMAÇÕES NA APLICAÇÃO SPARK --------
+      2.5 Escrita de base final em bucket do s3
+---------------------------------------------------"""
+
+# Transformando DataFrame em DynamicFrame
+logger.debug("Transformando DataFrame preparado em DynamicFrame")
+try:
+    dynamicf_sot_ecommerce = DynamicFrame.fromDF(
+        df_sot_ecommerce_partitioned,
+        glueContext,
+        "dynamicf_sot_ecommerce"
+    )
+except Exception as e:
+    logger.error("Erro ao transformar DataFrame em DynamicFrame. " +
+                 f"Exception: {e}")
+    raise e
+
+# Criando sincronização com bucket s3
+logger.debug("Preparando e sincronizando elementos de saída da tabela")
+try:
+    data_sink = glueContext.getSink(
+        path=OUTPUT_PATH,
+        connection_type=CONNECTION_TYPE,
+        updateBehavior=UPDATE_BEHAVIOR,
+        partitionKeys=PARTITION_KEYS,
+        compression=COMPRESSION,
+        enableUpdateCatalog=ENABLE_UPDATE_CATALOG,
+        transformation_ctx="data_sink",
+    )
+except Exception as e:
+    logger.error("Erro ao configurar elementos de saída via getSink. " +
+                 f"Exception: {e}")
+    raise e
+
+# Configurando informações do catálogo de dados
+logger.debug("Adicionando entrada para tabela no catálogo de dados")
+try:
+    data_sink.setCatalogInfo(
+        catalogDatabase=OUTPUT_DB,
+        catalogTableName=OUTPUT_TABLE
+    )
+    data_sink.setFormat("glueparquet")
+    data_sink.writeFrame(dynamicf_sot_ecommerce)
+except Exception as e:
+    logger.error("Erro ao adicionar entrada para tabela no catálogo " +
+                 f"de dados. Exception: {e}")
+    raise e
+
+logger.info("Commitando job")
+job.commit()
