c0ae741b09b61933e5e9a34d5c833119e56c749f
Thiago Panini <panini.development@gmail.com>
2023-04-11 20:57:25 -0300

    feat: starting glue Terraform file (#61)

---
+++ glue.tf
@@ -0,0 +1,11 @@
+/* --------------------------------------------------------
+FILE: glue.tf
+
+This is probably the main file in the whole module as it is
+used to specify and declare a preconfigured Glue job to be
+explored by users. This file considers the usage of all
+other componentes and resources already declared in other
+Terrafom files, such as IAM roles.
+-------------------------------------------------------- */
+
+# Defining a Glue job
