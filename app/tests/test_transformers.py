7e107455f66d2cec84bca52936eb9e8aabec8e57
Thiago Panini <panini.development@gmail.com>
2023-04-30 23:55:27 -0300

    fix: including the spark_session arg on transform_order_items call (#69)

--- app/tests/test_transformers.py
+++ app/tests/test_transformers.py
@@ -14,6 +14,7 @@ import pytest
 from pyspark.sql import DataFrame
 
 from tests.helpers.dataframes import compare_dataframe_schemas
+from tests.conftest import spark
 
 from src.transformers import transform_orders,\
     transform_order_items
@@ -104,4 +105,4 @@ def test_exception_on_transforming_df_order_items_dataframe():
     """
 
     with pytest.raises(Exception):
-        _ = transform_order_items(df=None)
+        _ = transform_order_items(df=None, spark_session=spark)
