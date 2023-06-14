d702240b9bd5bc8372cde77cf08d696bf52f3317
Thiago Panini <panini.development@gmail.com>
2023-06-13 21:16:13 -0300

    chore: changing the entry of "data" key from lists to tuples

--- app/tests/helpers/user_inputs.py
+++ app/tests/helpers/user_inputs.py
@@ -117,10 +117,10 @@ SOURCE_DATAFRAMES_DICT = {
             }
         ],
         "data": [
-            ["a4591c265e18cb1dcee52889e2d8acc3", 1, "060cb19345d90064d1015407193c233d", "8581055ce74af1daba164fdbd55a40de", "2017-07-13 22:10:13", 147.9, 27.36],
-            ["ad21c59c0840e6cb83a9ceb5573f8159", 1, "65266b2da20d04dbe00c5c2d3bb7859e", "2c9e548be18521d1c43cde1c582c6de8", "2018-02-19 20:31:37", 19.9, 8.72],
-            ["e481f51cbdc54678b7cc49136f2d6af7", 1, "87285b34884572647811a353c7ac498a", "3504c0cb71d7fa48d967e0e4c94d59d9", "2017-10-06 11:07:15", 29.99, 8.72],
-            ["e69bfb5eb88e0ed6a785585b27e16dbf", 1, "9a78fb9862b10749a117f7fc3c31f051", "7c67e1448b00f6e969d365cea6b010ab", "2017-08-11 12:05:32", 149.99, 19.77]
+            ("a4591c265e18cb1dcee52889e2d8acc3", 1, "060cb19345d90064d1015407193c233d", "8581055ce74af1daba164fdbd55a40de", "2017-07-13 22:10:13", 147.9, 27.36),
+            ("ad21c59c0840e6cb83a9ceb5573f8159", 1, "65266b2da20d04dbe00c5c2d3bb7859e", "2c9e548be18521d1c43cde1c582c6de8", "2018-02-19 20:31:37", 19.9, 8.72),
+            ("e481f51cbdc54678b7cc49136f2d6af7", 1, "87285b34884572647811a353c7ac498a", "3504c0cb71d7fa48d967e0e4c94d59d9", "2017-10-06 11:07:15", 29.99, 8.72),
+            ("e69bfb5eb88e0ed6a785585b27e16dbf", 1, "9a78fb9862b10749a117f7fc3c31f051", "7c67e1448b00f6e969d365cea6b010ab", "2017-08-11 12:05:32", 149.99, 19.77),
         ]
     },
     "tbl_brecommerce_customers": {
@@ -156,10 +156,10 @@ SOURCE_DATAFRAMES_DICT = {
             }
         ],
         "data": [
-            ["f54a9f0e6b351c431402b8461ea51999", "39382392765b6dc74812866ee5ee92a7", 0, "faxinalzinho", "RS"],
-            ["f88197465ea7920adcdbec7375364d82", "7c142cf63193a1473d2e66489a9ae977", 0, "sao goncalo do amarante", "RN"],
-            ["ed0271e0b7da060a393796590e7b737a", "36edbb3fb164b1f16485364b6fb04c73", 0, "santa rosa", "RS"],
-            ["b0830fb4747a6c6d20dea0b8c802d7ef", "af07308b275d755c9edb36a90c618231", 0, "barreiras", "BA"]
+            ("f54a9f0e6b351c431402b8461ea51999", "39382392765b6dc74812866ee5ee92a7", 0, "faxinalzinho", "RS"),
+            ("f88197465ea7920adcdbec7375364d82", "7c142cf63193a1473d2e66489a9ae977", 0, "sao goncalo do amarante", "RN"),
+            ("ed0271e0b7da060a393796590e7b737a", "36edbb3fb164b1f16485364b6fb04c73", 0, "santa rosa", "RS"),
+            ("b0830fb4747a6c6d20dea0b8c802d7ef", "af07308b275d755c9edb36a90c618231", 0, "barreiras", "BA")
         ]
     },
     "tbl_brecommerce_payments": {
@@ -195,13 +195,13 @@ SOURCE_DATAFRAMES_DICT = {
             }
         ],
         "data": [
-            ["e481f51cbdc54678b7cc49136f2d6af7", 1, "credit_card", 1, 18.12],
-            ["e69bfb5eb88e0ed6a785585b27e16dbf", 2, "voucher", 1, 161.42],
-            ["a4591c265e18cb1dcee52889e2d8acc3", 1, "credit_card", 6, 175.26],
-            ["e481f51cbdc54678b7cc49136f2d6af7", 3, "voucher", 1, 2.0],
-            ["e69bfb5eb88e0ed6a785585b27e16dbf", 1, "credit_card", 1, 8.34],
-            ["ad21c59c0840e6cb83a9ceb5573f8159", 1, "credit_card", 1, 28.62],
-            ["e481f51cbdc54678b7cc49136f2d6af7", 2, "voucher", 1, 18.59]
+            ("e481f51cbdc54678b7cc49136f2d6af7", 1, "credit_card", 1, 18.12),
+            ("e69bfb5eb88e0ed6a785585b27e16dbf", 2, "voucher", 1, 161.42),
+            ("a4591c265e18cb1dcee52889e2d8acc3", 1, "credit_card", 6, 175.26),
+            ("e481f51cbdc54678b7cc49136f2d6af7", 3, "voucher", 1, 2.0),
+            ("e69bfb5eb88e0ed6a785585b27e16dbf", 1, "credit_card", 1, 8.34),
+            ("ad21c59c0840e6cb83a9ceb5573f8159", 1, "credit_card", 1, 28.62),
+            ("e481f51cbdc54678b7cc49136f2d6af7", 2, "voucher", 1, 18.59)
         ]
     },
     "tbl_brecommerce_reviews": {
@@ -247,16 +247,16 @@ SOURCE_DATAFRAMES_DICT = {
             }
         ],
         "data": [
-            ["7bc2406110b926393aa56f80a40eba40", "73fc7af87114b39712e6da79b0a377eb", 4, "", "", "2018-01-18 00:00:00", "2018-01-18 21:46:59"],
-            ["80e641a11e56f04c1ad469d5645fdfde", "a548910a1c6147796b98fdf73dbeba33", 5, "", "", "2018-03-10 00:00:00", "2018-03-11 03:05:13"],
-            ["228ce5500dc1d8e020d8d1322874b6f0", "f9e4b658b201a9f2ecdecbb34bed034b", 5, "", "", "2018-02-17 00:00:00", "2018-02-18 14:36:24"],
-            ["e64fb393e7b32834bb789ff8bb30750e", "658677c97b385a9be170737859d3511b", 5, "", "Recebi bem antes do prazo estipulado.", "2017-04-21 00:00:00", "2017-04-21 22:02:06"],
-            ["f7c4243c7fe1938f181bec41a392bdeb", "8e6bfb81e283fa7e4f11123a3fb894f1", 5, "", "Parabéns lojas lannister adorei comprar pela Internet seguro e prático Parabéns a todos feliz Páscoa", "2018-03-01 00:00:00", "2018-03-02 10:26:53"],
-            ["15197aa66ff4d0650b5434f1b46cda19", "b18dcdf73be66366873cd26c5724d1dc", 1, "", "", "2018-04-13 00:00:00", "2018-04-16 00:39:37"],
-            ["07f9bee5d1b850860defd761afa7ff16", "e48aa0d2dcec3a2e87348811bcfdf22b", 5, "", "", "2017-07-16 00:00:00", "2017-07-18 19:30:34"],
-            ["7c6400515c67679fbee952a7525281ef", "c31a859e34e3adac22f376954e19b39d", 5, "", "", "2018-08-14 00:00:00", "2018-08-14 21:36:06"],
-            ["a3f6f7f6f433de0aefbb97da197c554c", "9c214ac970e84273583ab523dfafd09b", 5, "", "", "2017-05-17 00:00:00", "2017-05-18 12:05:37"],
-            ["8670d52e15e00043ae7de4c01cc2fe06", "b9bf720beb4ab3728760088589c62129", 4, "", "recomendo aparelho eficiente. no site a marca do aparelho esta impresso como 3desinfector e ao chegar esta com outro nome...atualizar com a marca correta uma vez que é o mesmo aparelho", "2018-05-22 00:00:00", "2018-05-23 16:45:47"]
+            ("7bc2406110b926393aa56f80a40eba40", "73fc7af87114b39712e6da79b0a377eb", 4, "", "", "2018-01-18 00:00:00", "2018-01-18 21:46:59"),
+            ("80e641a11e56f04c1ad469d5645fdfde", "a548910a1c6147796b98fdf73dbeba33", 5, "", "", "2018-03-10 00:00:00", "2018-03-11 03:05:13"),
+            ("228ce5500dc1d8e020d8d1322874b6f0", "f9e4b658b201a9f2ecdecbb34bed034b", 5, "", "", "2018-02-17 00:00:00", "2018-02-18 14:36:24"),
+            ("e64fb393e7b32834bb789ff8bb30750e", "658677c97b385a9be170737859d3511b", 5, "", "Recebi bem antes do prazo estipulado.", "2017-04-21 00:00:00", "2017-04-21 22:02:06"),
+            ("f7c4243c7fe1938f181bec41a392bdeb", "8e6bfb81e283fa7e4f11123a3fb894f1", 5, "", "Parabéns lojas lannister adorei comprar pela Internet seguro e prático Parabéns a todos feliz Páscoa", "2018-03-01 00:00:00", "2018-03-02 10:26:53"),
+            ("15197aa66ff4d0650b5434f1b46cda19", "b18dcdf73be66366873cd26c5724d1dc", 1, "", "", "2018-04-13 00:00:00", "2018-04-16 00:39:37"),
+            ("07f9bee5d1b850860defd761afa7ff16", "e48aa0d2dcec3a2e87348811bcfdf22b", 5, "", "", "2017-07-16 00:00:00", "2017-07-18 19:30:34"),
+            ("7c6400515c67679fbee952a7525281ef", "c31a859e34e3adac22f376954e19b39d", 5, "", "", "2018-08-14 00:00:00", "2018-08-14 21:36:06"),
+            ("a3f6f7f6f433de0aefbb97da197c554c", "9c214ac970e84273583ab523dfafd09b", 5, "", "", "2017-05-17 00:00:00", "2017-05-18 12:05:37"),
+            ("8670d52e15e00043ae7de4c01cc2fe06", "b9bf720beb4ab3728760088589c62129", 4, "", "recomendo aparelho eficiente. no site a marca do aparelho esta impresso como 3desinfector e ao chegar esta com outro nome...atualizar com a marca correta uma vez que é o mesmo aparelho", "2018-05-22 00:00:00", "2018-05-23 16:45:47")
         ]
     },
 }
